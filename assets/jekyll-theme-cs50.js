c71cde2482951658148b4f6cf3cdf59113015f4f
David J. Malan <malan@harvard.edu>
2019-08-18 15:38:41 -0400

    WIP

--- assets/jekyll-theme-cs50.js
+++ assets/jekyll-theme-cs50.js
@@ -48,7 +48,7 @@ $(document).on('DOMContentLoaded', function() {
         const play = $('<span class="fa-li"><i class="fas fa-play"></i></span>');
         const click = function(eventObject) {
             console.log("HERE");
-            const marker = $(element).attr('data-marker');
+            let marker = $(element).attr('data-marker');
             console.log(marker);
             if (marker === '+') {
                 $(element).attr('data-marker', '-');
