bc0e75817d2bfee95aba4e8956fe88ac7998d29b
David J. Malan <malan@harvard.edu>
2019-08-18 12:23:39 -0400

    WIP

--- assets/jekyll-theme-cs50.js
+++ assets/jekyll-theme-cs50.js
@@ -46,10 +46,10 @@ $(document).on('DOMContentLoaded', function() {
 
         // Prepend icon
         if ($(element).attr('data-marker') === '+') {
-            $(element).prepend('<span class="fa-li"><i class="fas fa-play fa-xs" style="vertical-align: 25%"></i></span>');
+            $(element).prepend('<span class="fa-li"><i class="fas fa-play" style="font-size: 50%; vertical-align: 25%"></i></span>');
         }
         else if ($(element).attr('data-marker') === '-') {
-            $(element).prepend('<span class="fa-li"><i class="fas fa-play fa-xs" style="transform: rotate(90deg); vertical-align: 25%;"></i></span>');
+            $(element).prepend('<span class="fa-li"><i class="fas fa-play" style="font-size: 50%; transform: rotate(90deg); vertical-align: 25%;"></i></span>');
         }
         else if ($(element).attr('data-marker') === '*') {
             $(element).prepend('<span class="fa-li"><i class="fas fa-circle" style="font-size: 25%; vertical-align: 25%;"></i></span>');
