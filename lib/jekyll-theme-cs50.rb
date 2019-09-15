64d25a658b2772e8fc66d6e4226e8b5b87a020c4
David J. Malan <malan@harvard.edu>
2019-09-14 22:21:03 -0400

    WIP

--- lib/jekyll-theme-cs50.rb
+++ lib/jekyll-theme-cs50.rb
@@ -1,7 +1,7 @@
 require "cgi"
 require "jekyll"
 require "kramdown/parser/gfm"
-require "liquid/tag/parser"
+#require "liquid/tag/parser"
 require "sanitize"
 require "uri"
 
@@ -13,9 +13,9 @@ module CS50
 
     def initialize(tag_name, markup, options)
       super
-      @args = Liquid::Tag::Parser.new(markup)
+      #@args = Liquid::Tag::Parser.new(markup)
       #alert = text.strip().gsub(/\A"|"\Z/, "").gsub(/\A"|"\Z/, "")
-      puts @args
+      #puts @args
       alert = "primary"
       @alert = (["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"].include? alert) ? alert : ""
     end
