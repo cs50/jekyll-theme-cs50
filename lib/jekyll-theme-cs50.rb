b29507c344145e568a9f0921b2da92718817b0cb
David J. Malan <malan@harvard.edu>
2019-09-15 00:11:29 -0400

    WIP

--- lib/jekyll-theme-cs50.rb
+++ lib/jekyll-theme-cs50.rb
@@ -99,6 +99,10 @@ module CS50
         # Determine aspect ratio
         @ratio = "16by9"
         ["21by9", "4by3", "1by1"].each do |ratio|
+          puts @args.args.keys[1]
+          puts "vs"
+          puts ratio
+          puts ""
           if @args.args.keys[1] == ratio
             puts "OVERRIDING"
             puts ratio
