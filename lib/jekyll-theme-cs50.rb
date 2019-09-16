0fc73411ee332ca2ad602141ffbfed9818fb806f
David J. Malan <malan@harvard.edu>
2019-09-16 00:46:31 -0400

    WIP

--- lib/jekyll-theme-cs50.rb
+++ lib/jekyll-theme-cs50.rb
@@ -207,32 +207,29 @@ end
 
 # Configure site
 Jekyll::Hooks.register :site, :after_reset do |site|
-
-  #
   site.config = Jekyll::Utils.deep_merge_hashes(Jekyll::Utils.deep_merge_hashes(CS50::DEFAULTS, site.config), CS50::OVERRIDES)
+end
 
-  # TODO
-  # https://github.com/benbalter/jekyll-relative-links/blob/master/lib/jekyll-relative-links/generator.rb
-  if site.baseurl
-    Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc, payload|
-      LINK_TEXT_REGEX = %r!(.*?)!
-      FRAGMENT_REGEX = %r!(#.+?)?!
-      INLINE_LINK_REGEX = %r!\[#{LINK_TEXT_REGEX}\]\(([^\)]+?)#{FRAGMENT_REGEX}\)!
-      markdown_converter ||= doc.site.find_converter_instance(Jekyll::Converters::Markdown)
-      if markdown_converter.matches(doc.extname)
-        doc.content = doc.content.dup.gsub(INLINE_LINK_REGEX) do |original|
-          a = Regexp.last_match[1]
-          href = Regexp.last_match[2]
-          if href.start_with?("/")
-            href = doc.site.baseurl + href
-            "[#{a}](#{href})"
-          else
-            original
-          end
-        end
-        puts doc.content
+# TODO
+# https://github.com/benbalter/jekyll-relative-links/blob/master/lib/jekyll-relative-links/generator.rb
+LINK_TEXT_REGEX = %r!(.*?)!.freeze
+FRAGMENT_REGEX = %r!(#.+?)?!.freeze
+INLINE_LINK_REGEX = %r!\[#{LINK_TEXT_REGEX}\]\(([^\)]+?)#{FRAGMENT_REGEX}\)!.freeze
+Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc, payload|
+  next if !doc.site.baseurl
+  markdown_converter ||= doc.site.find_converter_instance(Jekyll::Converters::Markdown)
+  if markdown_converter.matches(doc.extname)
+    doc.content = doc.content.dup.gsub(INLINE_LINK_REGEX) do |original|
+      a = Regexp.last_match[1]
+      href = Regexp.last_match[2]
+      if href.start_with?("/")
+        href = doc.site.baseurl + href
+        "[#{a}](#{href})"
+      else
+        original
       end
     end
+    puts doc.content
   end
 end
 
