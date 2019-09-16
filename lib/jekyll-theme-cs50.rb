37e71e2aa9b54cc98cb78e0ea1528272f256f837
David J. Malan <malan@harvard.edu>
2019-09-16 01:46:09 -0400

    WIP

--- lib/jekyll-theme-cs50.rb
+++ lib/jekyll-theme-cs50.rb
@@ -212,10 +212,11 @@ end
 LINK_TEXT_REGEX = %r!(.*?)!.freeze
 FRAGMENT_REGEX = %r!(#.+?)?!.freeze
 INLINE_LINK_REGEX = %r!\[#{LINK_TEXT_REGEX}\]\(([^\)]+?)#{FRAGMENT_REGEX}\)!.freeze
-Jekyll::Hooks.register [:documents], :pre_render do |doc, payload|
+Jekyll::Hooks.register [:pages], :post_render do |doc, payload|
 
   puts "HERE"
   puts doc
+  next
 
   # If no site.baseurl
   next if !doc.site.baseurl
