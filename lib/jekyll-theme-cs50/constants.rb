49b36a0d4361f0253838fed9adce828aec8b3e8e
David J. Malan <malan@harvard.edu>
2020-07-19 15:27:05 -0400

    including license.md, updated gems

--- lib/jekyll-theme-cs50/constants.rb
+++ lib/jekyll-theme-cs50/constants.rb
@@ -3,10 +3,10 @@ module CS50
   PLUGINS = {
     "jekyll-algolia" => "1.6.0",
     "jekyll-default-layout" => "0.1.4",
-    "jekyll-optional-front-matter" => "0.3.0",
-    "jekyll-redirect-from" => "0.15.0",
-    "jekyll-titles-from-headings" => "0.5.1",
-    "jemoji" => "0.11.1"
+    "jekyll-optional-front-matter" => "0.3.2",
+    "jekyll-redirect-from" => "0.16.0",
+    "jekyll-titles-from-headings" => "0.5.3",
+    "jemoji" => "0.12.0"
   }.freeze
 
   DEFAULTS = {
@@ -24,6 +24,9 @@ module CS50
       "Gemfile",
       "Gemfile.lock",
       "vendor"
+    ],
+    "include" => [
+      "license.md" # For OCW
     ],
     "kramdown" => {
       "gfm_quirks" => "paragraph_end",
